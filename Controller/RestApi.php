86f964e67972110e6c55c4fbb7d1037bf240f0d7
Broc Gailit <brocgailit@gmail.com>
2019-11-13 09:31:39 -0800

    Initial push for params

--- Controller/RestApi.php
+++ Controller/RestApi.php
@@ -4,28 +4,44 @@ namespace Rezdy\Controller;
 
 use \LimeExtra\Controller;
 use GuzzleHttp\Client;
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Exception\RequestException;
 
 class RestApi extends Controller {
 	private $api_key;
 	private $client;
+	private $options;
 
 	public function __construct($options) {
 		parent::__construct($options);
+		$this->options = $options;
 		$this->api_key = $this->app['config']['rezdy']['api_key'];
 		$this->client = new Client([
 			'base_uri' => 'https://api.rezdy.com/v1/products/'
 		]);
 	}
 
+	private function query($endpoint = '', $options = []) {
+		$response = $this->client->request('GET', $endpoint, [
+			'query' => array_merge(['apiKey' => $this->api_key], $options)
+		]);
+		return json_decode($response->getBody());
+	}
+
     public function products($id = '') {
-		$response = $this->client->request('GET', $id, ['query' => ['apiKey' => $this->api_key]]);
-		
+		// Accepted Query Parameters
+		$limit = $this->app->param('limit');
+		$search = $this->app->param('search');
+		$offset = $this->app->param('offset');
+
+		$query = $this->query($id);
+		$status = $query->requestStatus;
+
+		if ( !$status->success ) {
+			return $status;		
+		}
 		if ( !empty($id) ) {
-	        return ['products' => '$entry', 'id' => $this->api_key];
+		    return ['product' => $query->product];
 		} else {
-			return ['products' => $this->api_key];
+			return ['products' => $query->products];
 		}
     }
 
