e9a6f981e6816ec65adddade1b8a7584c5f7091b
Broc Gailit <brocgailit@gmail.com>
2019-11-12 17:09:13 -0800

    Move key assignment into class

--- Controller/RestApi.php
+++ Controller/RestApi.php
@@ -9,20 +9,22 @@ $client = new Client([
 	'base_uri' => 'https://api.rezdy.com/v1/products/'
 ]);
 
-$key = $this->app['config']['rezdy']['api_key'];
-
 // https://api.rezdy.com/v1/products/${productCode}?apiKey=${REZDY_KEY}
 
 class RestApi extends Controller {
+	$key = $this->app['config']['rezdy']['api_key'];
+
     public function products($id = '') {
 		// $entry = $this->app->module('collections')->findOne('Projects', ['_id' => $id]);
 
-
+		$response = $client->get($id, [
+			'query' => ['apiKey' => $key]
+		]);
 		
 		if ( !empty($id) ) {
-	        return ['products' => '$entry', 'id' => $key];
+	        return ['products' => '$entry', 'id' => $id];
 		} else {
-			return ['products' => $key];
+			return ['products' => $id];
 		}
     }
 
