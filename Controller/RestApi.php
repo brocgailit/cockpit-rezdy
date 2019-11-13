1f2bb23e4000080285527e037412e12b0c4f540f
Broc Gailit <brocgailit@gmail.com>
2019-11-12 16:47:00 -0800

    Test pulling from config

--- Controller/RestApi.php
+++ Controller/RestApi.php
@@ -3,16 +3,20 @@
 namespace Rezdy\Controller;
 
 use \LimeExtra\Controller;
+use GuzzleHttp\Client;
 
-// $client = new GuzzleHttp\Client();
+$client = new Client([
+	'base_uri' => 'https://rezd'
+]);
 
 class RestApi extends Controller {
     public function products($id) {
-        // $entry = $this->app->module('collections')->findOne('Projects', ['_id' => $id]);
+		// $entry = $this->app->module('collections')->findOne('Projects', ['_id' => $id]);
+		
 		if ( !empty($id) ) {
-	        return ['products' => '$entry', 'id' => $id];
+	        return ['products' => '$entry', 'id' => $this->app['config']];
 		} else {
-			return ['products' => 'farts'];
+			return ['products' => $this->app['config']];
 		}
     }
 
