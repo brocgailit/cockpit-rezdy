df9de153ea66aa7a755194f06122ab2cd609e65a
Broc Gailit <brocgailit@gmail.com>
2019-11-13 11:06:21 -0800

    Test 'default' funciton in class

--- Controller/ResourcesApi.php
+++ Controller/ResourcesApi.php
@@ -11,13 +11,16 @@ class ResourcesApi extends Controller {
 	public function __construct($options) {
         parent::__construct($options);
         $this->rezdy = new RezdyEndpoint('https://api.rezdy.com/v1/resources/', $this->app['config']['rezdy']['api_key']);
-	}
+    }
+    
+    public function __toString() {
+        return 'test';
+    }
 
     public function resources($resource_id = '') {
 
 		$res = $this->rezdy->query($resource_id, [
 			'limit' => $this->app->param('limit') ?: 100,
-			'search' => $this->app->param('search') ?: '',
 			'offset' => $this->app->param('offset') ?: 0,
 		]);
 
