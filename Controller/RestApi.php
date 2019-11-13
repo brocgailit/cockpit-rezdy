8e3b6350e1d385a4cb5f90acebaa70ba41382bea
Broc Gailit <brocgailit@gmail.com>
2019-11-12 17:00:22 -0800

    Rezdy testing

--- Controller/RestApi.php
+++ Controller/RestApi.php
@@ -6,17 +6,25 @@ use \LimeExtra\Controller;
 use GuzzleHttp\Client;
 
 $client = new Client([
-	'base_uri' => 'https://rezd'
+	'base_uri' => 'https://api.rezdy.com/v1/products'
 ]);
 
+$key = $this->app['config']['rezdy']['api_key'];
+
+// https://api.rezdy.com/v1/products/${productCode}?apiKey=${REZDY_KEY}
+
 class RestApi extends Controller {
-    public function products($id) {
+    public function products($id = '') {
 		// $entry = $this->app->module('collections')->findOne('Projects', ['_id' => $id]);
+
+		$products = $client->get($id, [
+			'query' => ['apiKey' => $key]
+		]);
 		
 		if ( !empty($id) ) {
-	        return ['products' => '$entry', 'id' => $this->app['config']['rezdy']['api_key']];
+	        return ['products' => '$entry', 'id' => $products];
 		} else {
-			return ['products' => $this->app['config']['rezdy']['api_key']];
+			return ['products' => $products];
 		}
     }
 
