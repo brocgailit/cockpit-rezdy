f313e1ef17b0c5d3fda12325af435649e1667b92
Broc Gailit <brocgailit@gmail.com>
2019-11-12 18:26:28 -0800

    Make things happen

--- Controller/RestApi.php
+++ Controller/RestApi.php
@@ -7,37 +7,25 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Exception\RequestException;
 
-// https://api.rezdy.com/v1/products/${productCode}?apiKey=${REZDY_KEY}
-
 class RestApi extends Controller {
-	private $key;
+	private $api_key;
 	private $client;
 
-	public function __construct() {
-		$this->$key = $this->app['config']['rezdy']['api_key'];
-		$this->$client = new Client([
+	public function __construct($options) {
+		parent::__construct($options);
+		$this->api_key = $this->app['config']['rezdy']['api_key'];
+		$this->client = new Client([
 			'base_uri' => 'https://api.rezdy.com/v1/products/'
 		]);
 	}
 
     public function products($id = '') {
-		// $entry = $this->app->module('collections')->findOne('Projects', ['_id' => $id]);
-
-		try {
-			$client->request('GET', 'https://api.rezdy.com/v1/products/', [
-				'query' => ['apiKey' => $this->$key]
-			]);
-		} catch (RequestException $e) {
-			return Psr7\str($e->getRequest());
-			if ($e->hasResponse()) {
-				return Psr7\str($e->getResponse());
-			}
-		}
+		$response = $this->client->request('GET', $id, ['query' => ['apiKey' => $this->api_key]]);
 		
 		if ( !empty($id) ) {
-	        return ['products' => '$entry', 'id' => $id];
+	        return ['products' => '$entry', 'id' => $this->api_key];
 		} else {
-			return ['products' => $id];
+			return ['products' => $this->api_key];
 		}
     }
 
