fe86ee391c888741c34cd245103f6d787a3f3d68
Broc Gailit <brocgailit@gmail.com>
2019-11-12 17:28:25 -0800

    Catch exception

--- Controller/RestApi.php
+++ Controller/RestApi.php
@@ -4,6 +4,8 @@ namespace Rezdy\Controller;
 
 use \LimeExtra\Controller;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
 
 // https://api.rezdy.com/v1/products/${productCode}?apiKey=${REZDY_KEY}
 
@@ -21,9 +23,16 @@ class RestApi extends Controller {
     public function products($id = '') {
 		// $entry = $this->app->module('collections')->findOne('Projects', ['_id' => $id]);
 
-		$response = $client->request('GET', 'https://api.rezdy.com/v1/products/', [
-			'query' => ['apiKey' => $this->$key]
-		]);
+		try {
+			$client->request('GET', 'https://api.rezdy.com/v1/products/', [
+				'query' => ['apiKey' => $this->$key]
+			]);
+		} catch (RequestException $e) {
+			return Psr7\str($e->getRequest());
+			if ($e->hasResponse()) {
+				return Psr7\str($e->getResponse());
+			}
+		}
 		
 		if ( !empty($id) ) {
 	        return ['products' => '$entry', 'id' => $id];
