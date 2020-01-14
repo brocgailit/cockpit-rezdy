a7bc44ff302fdcea689b1eab7cafc7cd4f028297
Broc Gailit <brocgailit@gmail.com>
2020-01-13 16:01:49 -0800

    Temporarily disable components

--- bootstrap.php
+++ bootstrap.php
@@ -18,6 +18,6 @@ if (COCKPIT_API_REQUEST) {
 }
 
 // ADMIN
-if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
+/* if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
   include_once(__DIR__ . '/admin.php');
-}
+} */
